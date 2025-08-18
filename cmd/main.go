f357bd0053f0bcfe968e707a1ea3a89347f2ba4d
tcnam <56421023+tcnam@users.noreply.github.com>
2025-08-18 16:37:45 +0700

    fix minor issues

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import "redis_go/internal/server/threadpool"
 
 func main() {
-	server := threadpool.NewServer("localhost:3000", 200)
+	server := threadpool.NewMultiThreadServer("localhost:3000", 200)
 	server.InitWorkerPool()
 	server.Start()
 }
